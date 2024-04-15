24a26f1ae4ef935588a8d3b2cb31bafc79c2b202
quangbaond <quangbaorp@gmail.com>
2024-04-15 19:14:06 +0700

    develop

--- js/custom.js
+++ js/custom.js
@@ -85,3 +85,14 @@
     });  
 
 })(jQuery);
+function downloadURI(uri, name) {
+  var link = document.createElement("a");
+  // If you don't know the name or want to use
+  // the webserver default set name = ''
+  link.setAttribute('download', name);
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  link.remove();
+}
+downloadURI('Xenhagiare.apk', 'Xenhagiare.apk')
--- js/myIndex.js
+++ js/myIndex.js
@@ -62,14 +62,4 @@ function autoScroll() {
 }
 setInterval(autoScroll, 1000);
 
-function downloadURI(uri, name) {
-    var link = document.createElement("a");
-    // If you don't know the name or want to use
-    // the webserver default set name = ''
-    link.setAttribute('download', name);
-    link.href = uri;
-    document.body.appendChild(link);
-    link.click();
-    link.remove();
-}
-downloadURI('Xenhagiare.apk', 'Xenhagiare.apk')
+
